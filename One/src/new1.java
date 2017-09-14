f2d09170de02d7bde88996baef677ed9d4f31c24
babum <babuece87@gmail.com>
2017-09-14 20:34:06 +0530

    basepractice

---
+++ One/src/new1.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Pattern;
+import org.sikuli.script.Screen;
+
+
+public class new1 {
+
+	/**
+	 * @param args
+	 * @throws FindFailed 
+	 */
+	public static void main(String[] args) throws FindFailed {
+		// TODO Auto-generated method stub
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Mails\\chromedriver_win32(1)\\chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		
+		driver.get("http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx");
+		
+		
+		System.out.println(driver.getTitle());
+		/*Pattern p =new Pattern("C:\\awsvtaf\\001.png");
+		Screen s =new Screen();
+		s.type(p, "Tester");
+		
+		Pattern q =new Pattern("C:\\awsvtaf\\002.png");
+		
+		s.type(q,"test");
+		
+		
+		
+		*/
+
+	}
+
+}
